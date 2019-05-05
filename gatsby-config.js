9c4c43ac0affd71f2a6648b620d0a9f6b49fa88f
Fumiya Goto <gotch@styz.io>
2019-05-05 12:36:23 +0900

    GraphQL with Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description:
+      'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords:
+      'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'b5k3rxtrrkf2',
+        accessToken: 'aw2h7eYPU8Ac7U-0NHALkhy_U7Qpf-kde1HBf50taU4'
+      }
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Card from '../components/Card'
 import Section from '../components/Section'
-import Cell from '../components/Cell'
 import Wave from '../components/Wave'
 import staticdata from '../../staticdata.json'
+import Cell from '../components/Cell'
 import styled from 'styled-components'
 
 const SectionCaption = styled.p`
@@ -18,10 +18,10 @@ const SectionCaption = styled.p`
 const SectionCellGroup = styled.div`
   max-width: 800px;
   margin: 0 auto 100px;
-  padding: 0 20px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-column-gap: 20px;
+  padding: 0 20px;
 
   @media (max-width: 800px) {
     grid-template-columns: repeat(1, 1fr);
@@ -40,55 +40,55 @@ const IndexPage = () => (
           Complete courses about the best tools and design systems. Prototype
           and build apps with React and Swift.
         </p>
-        <Link to="/video">Watch the video</Link>
-        <Wave />
+        <Link to="/page-2/">Watch the video</Link>
         <div className="Logos">
-          <img src={require('../images/logo-sketch.png')} width="50" />
-          <img src={require('../images/logo-figma.png')} width="50" />
-          <img src={require('../images/logo-studio.png')} width="50" />
-          <img src={require('../images/logo-framer.png')} width="50" />
-          <img src={require('../images/logo-react.png')} width="50" />
-          <img src={require('../images/logo-swift.png')} width="50" />
+          <img src="/images/logo-sketch.png" width="50" />
+          <img src="/images/logo-figma.png" width="50" />
+          <img src="/images/logo-studio.png" width="50" />
+          <img src="/images/logo-framer.png" width="50" />
+          <img src="/images/logo-react.png" width="50" />
+          <img src="/images/logo-swift.png" width="50" />
         </div>
+        <Wave />
       </div>
-      <div className="Cards">
-        <h2>11 courses, more coming</h2>
-        <div className="CardGroup">
-          <Card
-            title="DesignSystem"
-            text="10 sections"
-            image={require('../images/wallpaper.jpg')}
-          />
-          <Card
-            title="React for Designers"
-            text="11 sections"
-            image={require('../images/wallpaper2.jpg')}
-          />
-          <Card
-            title="Sound Design"
-            text="5 sections"
-            image={require('../images/wallpaper3.jpg')}
-          />
-          <Card
-            title="ARKit 2"
-            text="10 sections"
-            image={require('../images/wallpaper4.jpg')}
-          />
-        </div>
-      </div>
-      <Section
-        image={require('../images/wallpaper2.jpg')}
-        logo={require('../images/logo-react.png')}
-        title="React for Designers"
-        text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
-      />
-      <SectionCellGroup>
-        <SectionCaption>12 sections - 6 hours</SectionCaption>
-        {staticdata.cells.map(cell => (
-          <Cell title={cell.title} image={cell.image} />
-        ))}
-      </SectionCellGroup>
     </div>
+    <div className="Cards">
+      <h2>11 courses, more coming</h2>
+      <div className="CardGroup">
+        <Card
+          title="Design System"
+          text="10 sections"
+          image="/images/wallpaper.jpg"
+        />
+        <Card
+          title="React for Designers"
+          text="12 sections"
+          image="/images/wallpaper2.jpg"
+        />
+        <Card
+          title="Sound Design"
+          text="5 sections"
+          image="/images/wallpaper3.jpg"
+        />
+        <Card
+          title="ARKit 2"
+          text="10 sections"
+          image="/images/wallpaper4.jpg"
+        />
+      </div>
+    </div>
+    <Section
+      image="/images/wallpaper2.jpg"
+      logo="/images/logo-react.png"
+      title="React for Designers"
+      text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify."
+    />
+    <SectionCaption>12 sections - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {staticdata.cells.map(cell => (
+        <Cell title={cell.title} image={cell.image} />
+      ))}
+    </SectionCellGroup>
   </div>
 )
 
