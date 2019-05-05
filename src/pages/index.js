b2955b825137076828a3b74619d15cad8ee878b0
Fumiya Goto <gotch@styz.io>
2019-05-05 10:30:41 +0900

    Components and Props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card'
 
 const IndexPage = () => (
   <div>
@@ -36,6 +37,31 @@ const IndexPage = () => (
           <img src={require('../images/logo-framer.png')} width="50" />
           <img src={require('../images/logo-react.png')} width="50" />
           <img src={require('../images/logo-swift.png')} width="50" />
+        </div>
+        <div className="Cards">
+          <h2>11 courses, more coming</h2>
+          <div className="CardGroup">
+            <Card
+              title="DesignSystem"
+              text="10 sections"
+              image={require('../images/wallpaper.jpg')}
+            />
+            <Card
+              title="React for Designers"
+              text="11 sections"
+              image={require('../images/wallpaper2.jpg')}
+            />
+            <Card
+              title="Sound Design"
+              text="5 sections"
+              image={require('../images/wallpaper3.jpg')}
+            />
+            <Card
+              title="ARKit 2"
+              text="10 sections"
+              image={require('../images/wallpaper4.jpg')}
+            />
+          </div>
         </div>
       </div>
     </div>
